242eb979ff48fc6d01ccb52db18ee71c8ff3a752
prades.mathew <prades.mathew@gmail.com>
2024-06-09 14:22:56 +0000

    ajout de test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,11 +2,9 @@ public class HelloWorld {
  
 	public static void main(String[] args) {
 		System.out.println("Ciao mondo !");
-<<<<<<< HEAD
-		System.out.println("Olá mundo !");
-=======         System.out.println("Bonjour le monde !");
+		System.out.println("Hello World !");
 		System.out.println("Hallo Welt !");
->>>>>>> 211b969cdc5454d4ba5b311ce96d5a71bf36313f
+		
 		System.out.println("Hola Mundo !");
 	}
  
---
+++ src/test.py
@@ -0,0 +1,3 @@
+prenom = input("Entrez un prénom : ")
+nombre_lettres = len(prenom)
+print("Le prénom", prenom, "contient", nombre_lettres, "lettres.")
